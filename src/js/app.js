83eb51e1ce5ad83ed024eda7dddbcf871baf9597
lolamindi <laura_mday@yahoo.co.uk>
2023-11-17 19:39:35 +0100

    add some javascript functions

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+const redColor = document.querySelector(".red");
+const blackColor = document.querySelector(".black");
+const grayColor = document.querySelector(".gray");
+
+const cartButton = document.getElementById("button");
+const itemTag = document.getElementsByTagName("h3");
+const imgCard = document.querySelector(".product-image");
+
+redColor.addEventListener("click", () => {
+  imgCard.style.backgroundImage = 'url("../../public/img/redcar.jpg")';
+  cartButton.style.backgroundColor = "red";
+  itemTag.style.backgroundColor = "red";
+});
+
+blackColor.addEventListener("click", () => {
+  imgCard.style.backgroundImage = 'url("../../public/img/blackcar.jpg")';
+  cartButton.style.backgroundColor = "black";
+  itemTag.style.backgroundColor = "black";
+});
+
+grayColor.addEventListener("click", () => {
+  imgCard.style.backgroundImage = 'url("../../public/img/graycar.jpg")';
+  cartButton.style.backgroundColor = "gray";
+  itemTag.style.backgroundColor = "gray";
+});
+
+cartButton.addEventListener("click", () =>
+  alert("Confirmas tu compra?")
+);
